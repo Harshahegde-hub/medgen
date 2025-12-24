f708fdb74c9e1a05fc9a77f8dffecd6506411ced
Harshahegde-hub <mr.harshahegde@gmail.com>
2025-12-24 21:21:08 +0530

    Update and rename rx_ocr.py to ocr.py

---
+++ ocr.py
@@ -0,0 +1,18 @@
+import cv2
+import pytesseract
+
+def extract_text(image_path):
+    img = cv2.imread(image_path)
+
+    if img is None:
+        raise ValueError("Image not found")
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)[1]
+
+    text = pytesseract.image_to_string(
+        gray,
+        config="--psm 6"
+    )
+
+    return text
